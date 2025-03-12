1e94f4c28976e14ce3321ed3e4f1aa4d2f51de77
Neoly Moreno <neolyamr@gmail.com>
2025-03-12 18:12:41 -0500

    - Update repositories

--- src/main/java/com/neoapps/library_management_system/repositories/BillRepository.java
+++ src/main/java/com/neoapps/library_management_system/repositories/BillRepository.java
@@ -2,7 +2,9 @@ package com.neoapps.library_management_system.repositories;
 
 import com.neoapps.library_management_system.entities.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BillRepository extends JpaRepository<Bill, Long> {
 
 }
